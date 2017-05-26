ac915688efc97a8528f13a03a934b06c7ce44a10
Iván Rodríguez Murillo <wantez@gmail.com>
2017-05-26 22:15:31 +0200

    WIP 3

--- PresenceService.js
+++ PresenceService.js
@@ -1,58 +1,54 @@
-const EventBus = require('weplay-common').EventBus;
-const redis = require('weplay-common').redis();
-const io = require('socket.io-emitter')(redis);
-const interval = process.env.WEPLAY_INTERVAL || 5000;
-const logger = require('weplay-common').logger('weplay-presence');
+const EventBus = require('weplay-common').EventBus
+const redis = require('weplay-common').redis()
+require('socket.io-emitter')(redis)
+const interval = process.env.WEPLAY_INTERVAL || 5000
 
 class PresenceService {
+  constructor(discoveryUrl, discoveryPort) {
+    this.uuid = require('node-uuid').v4()
+    this.logger = require('weplay-common').logger('weplay-presence-service', this.uuid)
 
-    constructor(discoveryUrl, discoveryPort) {
-        this.uuid = require('node-uuid').v4();
-        this.logger = require('weplay-common').logger('weplay-presence-service', this.uuid);
-
-        setInterval(() => {
-            redis.hgetall('weplay:connections', (err, counts) => {
-                if (!counts) {
-                    return;
-                }
-                this.broadcastData(counts);
-            });
-        }, interval);
-
-        this.bus = new EventBus({
-            url: discoveryUrl,
-            port: discoveryPort,
-            name: 'presence',
-            id: this.uuid
-        }, ()=> {
-            this.logger.info('PresenceService connected to discovery server', {
-                discoveryUrl: discoveryUrl,
-                uuid: this.uuid
-            });
-            this.init();
-        });
-    }
-
-    init() {
-        this.logger.info('PresenceService init()');
-    }
-
-    broadcastData(counts) {
-        let count = 0;
-        for (const i in counts) {
-            if (counts.hasOwnProperty(i)) {
-                count += Number(counts[i]);
-            }
+    setInterval(() => {
+      redis.hgetall('weplay:connections', (err, counts) => {
+        if (!counts || err) {
+          return
         }
-        this.logger.debug('connections', count);
-        redis.set('weplay:connections-total', count);
-        this.bus.emit('connections', count);
-    }
+        this.broadcastData(counts)
+      })
+    }, interval)
 
-    destroy() {
-        this.logger.info('PresenceService destroy()');
+    this.bus = new EventBus({
+      url: discoveryUrl,
+      port: discoveryPort,
+      name: 'presence',
+      id: this.uuid
+    }, () => {
+      this.logger.info('PresenceService connected to discovery server', {
+        discoveryUrl: discoveryUrl,
+        uuid: this.uuid
+      })
+      this.init()
+    })
+  }
+
+  init() {
+    this.logger.info('PresenceService init()')
+  }
+
+  broadcastData(counts) {
+    let count = 0
+    for (const i in counts) {
+      if (counts.hasOwnProperty(i)) {
+        count += Number(counts[i])
+      }
     }
+    this.logger.debug('connections', count)
+    redis.set('weplay:connections-total', count)
+    this.bus.emit('connections', count)
+  }
+
+  destroy() {
+    this.logger.info('PresenceService destroy()')
+  }
 }
-module.exports = PresenceService;
-
-
+module.exports = PresenceService
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-process.title = 'weplay-presence';
+process.title = 'weplay-presence'
 
-const discoveryUrl = process.env.DISCOVERY_URL || 'http://localhost:3010';
-const discoveryPort = process.env.DISCOVERY_PORT || 3060;
+const discoveryUrl = process.env.DISCOVERY_URL || 'http://localhost:3010'
+const discoveryPort = process.env.DISCOVERY_PORT || 3060
 
-const PresenceService = require('./PresenceService');
-const service = new PresenceService(discoveryUrl, discoveryPort);
+const PresenceService = require('./PresenceService')
+const service = new PresenceService(discoveryUrl, discoveryPort)
 
-require('weplay-common').cleanup(service.destroy.bind(service));
+require('weplay-common').cleanup(service.destroy.bind(service))
